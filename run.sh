864d5583531bcbb0f75f50f2d3dc1fe37d9ba2e1
88plug <19512127+88plug@users.noreply.github.com>
2020-10-29 04:30:28 -0700

    Update and rename start.sh to run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,7 @@ git clone https://github.com/cryptoandcoffee/lightning-node
 git clone https://github.com/cryptoandcoffee/lightning
 mkdir btc-data/
 mkdir lightning-data/
-docker-compose up -d --force-recreate
+docker-compose up -d btc
+echo "Sleeping 15 seconds for BTC init"
+sleep 15
+docker-compose up -d ln
