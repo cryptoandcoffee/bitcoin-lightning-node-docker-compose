54cef0f81d4fd87f6fcadbb1b951eb51a8bdd610
88plug <19512127+88plug@users.noreply.github.com>
2020-10-29 03:46:02 -0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/cryptoandcoffee/lightning-node
+git clone https://github.com/cryptoandcoffee/lightning
+mkdir btc-data/
+mkdir lightning-data/
+docker-compose up -d --force-recreate
